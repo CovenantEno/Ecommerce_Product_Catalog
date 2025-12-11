99687ce3a7f2cfbc697304fd4b97977045ef048a
Edogi Covenant <125829124+CovenantEno@users.noreply.github.com>
2025-12-11 03:22:50 -0800

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -42,7 +42,7 @@ def login_view(request):
     else:
         form = LoginForm()
 
-    return render(request, 'accounts/login.html', {'form': form})
+    return render(request, 'accounts/home.html', {'form': form})
 
 def logout_view(request):
     logout(request)

91cbfbb2636d160528eebd68ead5e42682d9c7f0
Edogi Covenant <125829124+CovenantEno@users.noreply.github.com>
2025-12-11 02:49:49 -0800

    Update settings.py

--- Ecommerce_Catalog/settings.py
+++ Ecommerce_Catalog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jvr199tl*&$!tpn&6usij@ifhd77or7t4+b0+sgni_tf@g6n^z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
